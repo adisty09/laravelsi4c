dfa0ffa3ee8361994707f7efd8f8f279b3278d7a
firaramaita <ramaita91@gmail.com>
2026-05-05 17:24:11 +0700

    bootstrap pt 5 mei

---
+++ laravelsi4c/resources/views/fakultas/create.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+</head>
+<body>
+    <form action="/fakultas" method="post">
+         <div class="mb-3">
+            <h5>Form Pengisian Data Fakultas</h5>
+            <label for="nama" class="form-label">Nama Fakultas</label>
+            <input type="email" class="form-control" id="nama" placeholder="Masukkan nama fakultas">
+</div>
+
+
+    </form>
+   
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script> 
+</body>
+</html>
